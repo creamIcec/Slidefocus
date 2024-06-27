2ed38f5c6798d18f292bcfee0fd8f08dbc6ec499
Iriseplosc <42510470+creamIcec@users.noreply.github.com>
2024-06-27 20:46:47 +0800

    update: like logic rebuild

--- Slidefocus/src/renderer/components/LikeButton.tsx
+++ Slidefocus/src/renderer/components/LikeButton.tsx
@@ -9,27 +9,16 @@ export default function LikeButton({
   imagePath: string;
   liked: boolean;
 }) {
-  const [internalLiked, setInternalLiked] = useState<boolean>(liked);
-
-  const onClickVisualFunction = () => {
-    setInternalLiked(!internalLiked);
-  };
-
-  useEffect(() => {
-    setInternalLiked(liked);
-  }, [liked]);
-
   return (
     <button
       onClick={() => {
-        onToggle(imagePath, !internalLiked, '');
-        onClickVisualFunction();
+        onToggle(imagePath, !liked, '');
       }}
       className="rounded-3xl bg-bg_dark/40 dark:bg-bg/40 backdrop-blur-sm w-8 h-8 p-6 flex flex-nowrap place-content-center place-items-center"
     >
       <i
         className={`${
-          internalLiked
+          liked
             ? 'material-icons text-red-400'
             : 'material-symbols-outlined text-black dark:text-white'
         } text-4xl  `}
